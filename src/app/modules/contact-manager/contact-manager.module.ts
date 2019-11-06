cef7831a8d3a78a8dfc2da2ce617fd7aae86560d
Carl Fearby <carlfearby@me.com>
2019-11-06 00:47:56 +0000

    removed the json pipe in the sub module

--- src/app/modules/contact-manager/contact-manager.module.ts
+++ src/app/modules/contact-manager/contact-manager.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule} from '@angular/common';
 import { MainComponent } from './main.component';
-import { JsonPipe } from '../../pipes/json.pipe';
 
 
 
 @NgModule({
-  declarations: [MainComponent, JsonPipe],
+  declarations: [MainComponent],
   imports: [
     CommonModule
   ]
