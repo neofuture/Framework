1410fbbb2b98c57bcd9445683a58abc1489194c0
Carl Fearby <carlfearby@me.com>
2019-11-13 15:42:47 +0000

    some menu changes

--- src/app/components/titlebar/titlebar.component.ts
+++ src/app/components/titlebar/titlebar.component.ts
@@ -11,40 +11,6 @@ import {RibbonButtonModel} from '../../models/ribbon-button-model';
   styleUrls: ['./titlebar.component.css']
 })
 export class TitleBarComponent implements OnInit {
-  objectKeys = Object.keys;
-
-  @Input() desktopWidth: number;
-  @Input() desktopHeight: number;
-  @Output() changedSize = new EventEmitter<boolean>();
-
-  icon = 'locationPin';
-  iconArray = [
-    'alarm', 'bin', 'catalogue_over', 'catalogue', 'catalogues_over',
-    'catalogues', 'clipboard', 'clock', 'cloud', 'cog_over', 'cog',
-    'company_over', 'company', 'computer', 'contacts_over', 'contacts',
-    'content_area', 'creditcard', 'dashboard_over', 'dashboard', 'diary_over',
-    'diary', 'downArrow', 'download', 'duplicate_page', 'email',
-    'exclamation_mark_circle', 'external_link', 'eye', 'fat_close', 'fat_tick',
-    'folder', 'forms', 'formsResponses', 'framework', 'funnel_over', 'funnel',
-    'ghost_over', 'ghost', 'globe', 'home_over', 'home', 'leftArrow',
-    'leftArrowStart', 'link', 'locationPin', 'lock_closed', 'lock_open',
-    'media', 'menu_over', 'menu', 'messages_over', 'messages', 'minus',
-    'move', 'orders_over', 'orders', 'page_add', 'page_settings', 'page_tags',
-    'pages', 'pencil', 'phone_over', 'phone', 'piechart_over', 'piechart',
-    'playbutton', 'plus', 'print_over', 'print', 'question_mark_circle',
-    'question_mark', 'quotations_over', 'quotations', 'reminder_over',
-    'reminder', 'reorder', 'rightArrow', 'rightArrowEnd', 'save', 'search_over',
-    'search', 'styles', 'tasks_over', 'tasks', 'templates', 'tree_corner',
-    'tree_line_horizontal', 'tree_line', 'tree_t', 'upArrow', 'users_over',
-    'users', 'world', 'oceanworks', 'minimise', 'restore', 'maximise', 'close'];
-
-  text = '#c1c1c1';
-
-  language$: Subscription;
-  locale: LanguageModel;
-
-  ribbonButtons: RibbonButtonModel[];
-  size = true;
 
   constructor(
     private windowService: WindowService,
@@ -53,15 +19,127 @@ export class TitleBarComponent implements OnInit {
   ) {
   }
 
+  objectKeys = Object.keys;
+
+  @Input() desktopWidth: number;
+  @Input() desktopHeight: number;
+  @Output() changedSize = new EventEmitter<boolean>();
+
+  icon = 'locationPin';
+  language$: Subscription;
+  locale: LanguageModel;
+
+  ribbonButtons: RibbonButtonModel[];
+  size = true;
+  barWidth = 'toolChange ow-upArrow';
+
   ngOnInit() {
 
     this.language$ = this.languageService.object.subscribe(locale => {
       this.locale = locale;
     });
 
-    console.log(this.el.nativeElement.offsetHeight);
-
     this.ribbonButtons = [{
+      icon: 'ow-contacts',
+      iconOver: 'ow-contacts_over',
+      label: 'contactManager',
+      module: 'contactManager'
+    }, {
+      icon: 'ow-quotations',
+      iconOver: 'ow-quotations_over',
+      label: 'quotes',
+      module: 'quotes'
+    }, {
+      icon: 'ow-cog',
+      iconOver: 'ow-cog_over',
+      label: 'settings',
+      module: 'settings'
+    }, {
+      icon: 'ow-messages',
+      iconOver: 'ow-messages_over',
+      label: 'messages',
+      module: 'messages'
+    }, {
+      icon: 'ow-contacts',
+      iconOver: 'ow-contacts_over',
+      label: 'contactManager',
+      module: 'contactManager'
+    }, {
+      icon: 'ow-quotations',
+      iconOver: 'ow-quotations_over',
+      label: 'quotes',
+      module: 'quotes'
+    }, {
+      icon: 'ow-cog',
+      iconOver: 'ow-cog_over',
+      label: 'settings',
+      module: 'settings'
+    }, {
+      icon: 'ow-messages',
+      iconOver: 'ow-messages_over',
+      label: 'messages',
+      module: 'messages'
+    }, {
+      icon: 'ow-contacts',
+      iconOver: 'ow-contacts_over',
+      label: 'contactManager',
+      module: 'contactManager'
+    }, {
+      icon: 'ow-quotations',
+      iconOver: 'ow-quotations_over',
+      label: 'quotes',
+      module: 'quotes'
+    }, {
+      icon: 'ow-cog',
+      iconOver: 'ow-cog_over',
+      label: 'settings',
+      module: 'settings'
+    }, {
+      icon: 'ow-messages',
+      iconOver: 'ow-messages_over',
+      label: 'messages',
+      module: 'messages'
+    }, {
+      icon: 'ow-contacts',
+      iconOver: 'ow-contacts_over',
+      label: 'contactManager',
+      module: 'contactManager'
+    }, {
+      icon: 'ow-quotations',
+      iconOver: 'ow-quotations_over',
+      label: 'quotes',
+      module: 'quotes'
+    }, {
+      icon: 'ow-cog',
+      iconOver: 'ow-cog_over',
+      label: 'settings',
+      module: 'settings'
+    }, {
+      icon: 'ow-messages',
+      iconOver: 'ow-messages_over',
+      label: 'messages',
+      module: 'messages'
+    }, {
+      icon: 'ow-contacts',
+      iconOver: 'ow-contacts_over',
+      label: 'contactManager',
+      module: 'contactManager'
+    }, {
+      icon: 'ow-quotations',
+      iconOver: 'ow-quotations_over',
+      label: 'quotes',
+      module: 'quotes'
+    }, {
+      icon: 'ow-cog',
+      iconOver: 'ow-cog_over',
+      label: 'settings',
+      module: 'settings'
+    }, {
+      icon: 'ow-messages',
+      iconOver: 'ow-messages_over',
+      label: 'messages',
+      module: 'messages'
+    }, {
       icon: 'ow-contacts',
       iconOver: 'ow-contacts_over',
       label: 'contactManager',
@@ -84,13 +162,16 @@ export class TitleBarComponent implements OnInit {
     }];
   }
 
-
-  getIcon() {
-    return this.iconArray[Math.floor(Math.random() * this.iconArray.length)];
-  }
-
   toggleSize() {
     this.size = !this.size;
+    this.barWidth = 'toolChange';
+    this.barWidth += this.size ? ' ow-upArrow' : ' ow-downArrow';
     this.changedSize.emit(this.size);
   }
+
+  changeStyle(event: MouseEvent) {
+    this.barWidth = event.type === 'mouseover' ? 'toolChange toolChangeWide' : 'toolChange';
+    this.barWidth += this.size ? ' ow-upArrow' : ' ow-downArrow';
+  }
+
 }
