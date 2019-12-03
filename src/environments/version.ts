0a1d5c6a399a4395844e55fbad9bd6d6e5350adc
Carl Fearby <carlfearby@me.com>
2019-12-03 13:56:43 +0000

    New version file generated

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,13 +2,13 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "54886dd-dirty",
-    "hash": "54886dd",
+    "raw": "ca49cde-dirty",
+    "hash": "ca49cde",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "54886dd-dirty",
+    "suffix": "ca49cde-dirty",
     "semverString": null,
-    "version": "0.1.1"
+    "version": "0.1.3"
 };
 /* tslint:enable */
